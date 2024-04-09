84b70837628c86117cbfad12738e7917088bdcc5
Lozo214 <blenderbook@arizona.edu>
2024-04-09 11:47:17 -0700

    Added new packages and classes

---
+++ src/controllers/MemoryGame_GUI.java
@@ -0,0 +1,27 @@
+package controllers;
+
+/**
+ 
+ */
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
+public class MemoryGame_GUI extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public void start(Stage stage) {
+
+	}
+
+}
